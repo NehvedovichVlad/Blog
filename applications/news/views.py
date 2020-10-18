049657269111c6a0f5aec3ee3a611a98c64d51fd
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-10-18 19:33:53 +0300

    templates

--- applications/news/views.py
+++ applications/news/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import News
+
 
 def index(request):
-    return HttpResponse("Hello world")
+    news = News.objects.order_by('-created_at')
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
