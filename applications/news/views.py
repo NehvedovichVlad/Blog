cc4eac1f62d7aed5fb0c05e2b36660923aff8ca1
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-10-21 17:12:20 +0300

    add button readmore, get_absolute_url,404

--- applications/news/views.py
+++ applications/news/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import News, Category
 
@@ -22,3 +21,10 @@ def get_category(request, category_id):
     }
     return render(request, 'news/category.html', context=context)
 
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    context = {
+        'news_item': news_item,
+    }
+    return render(request, 'news/view_news.html', context)
