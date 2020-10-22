dc123913a7ac7bb2e6fffb624d76bc911585a6fa
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-10-22 22:23:00 +0300

    add custom validator/title NewsForm

--- applications/news/forms.py
+++ applications/news/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import News
+
+import re
 
 
 class NewsForm(forms.ModelForm):
@@ -11,3 +14,9 @@ class NewsForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'category': forms.Select(attrs={'class': 'form-control'}),
         }
+        
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return  title
