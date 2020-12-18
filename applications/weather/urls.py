9f332d73606ac90ec9de0dd61fb4b3a60fdab2f1
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-12-18 16:56:52 +0300

    weather upgrade

--- applications/weather/urls.py
+++ applications/weather/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path("weather/", find_weather, name='weather'),
+    path("weather/delete/<city_name>/", delete_city, name='delete_city'),
 
 ]
--- applications/weather/views.py
+++ applications/weather/views.py
@@ -1,17 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 
-from .forms import CityForm
-from .models import City
+from applications.weather.forms import CityForm
+from applications.weather.models import City
 
 
 def find_weather(request):
     appid = '1547f56cb0f7d14c2d8fee4263efc7a6'
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
+    err_msg =''
+    message = ''
+    message_class = ''
+
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+
+            if existing_city_count == 0:
+                res = requests.get(url.format(new_city)).json()
+                if res['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist in the world'
+            else:
+                err_msg = 'City already exists in the database!'
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added successfully'
+            message_class = 'is-success'
+
 
     form = CityForm()
 
@@ -20,6 +43,7 @@ def find_weather(request):
 
     for city in cities:
         res = requests.get(url.format(city.name)).json()
+
         city_info = {
             'city': city.name,
             'temp': res["main"]["temp"],
@@ -29,10 +53,16 @@ def find_weather(request):
 
     context = {
         'all_info': all_cities,
-        'form': form
+        'form': form,
+        'message': message,
+        'message_class': message_class
     }
     return render(request, "weather/weather.html", context)
 
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    
+    return  redirect('weather')
 
     # image = {
     #     'Tokyo': 'Tokyo',
