ae1427262dedb1e221e9b2a75bd692bd31595b8f
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2020-12-21 18:28:12 +0300

    aws settings one

--- blog_project/settings/production.py
+++ blog_project/settings/production.py
@@ -205,7 +205,5 @@ AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
 AWS_S3_ADDRESSING_STYLE = "path"
 AWS_DEFAULT_ACL = None
-AWS_S3_OBJECT_PARAMETERS = {"ACL": "public-read"}
-
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
