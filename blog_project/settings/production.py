e25b80fda69cfcd41a2f6d4418eb837524012ba4
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2021-01-25 23:29:44 +0300

    aws

--- blog_project/settings/production.py
+++ blog_project/settings/production.py
@@ -198,9 +198,3 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
